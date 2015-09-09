3f24a2eccee69d6f4933fbe13fa1df454a934a0b
DevilsEyes <DevilsEyes@git.com>
2015-09-09 23:26:09 +0800

    v34

--- init.js
+++ init.js
@@ -12,41 +12,26 @@ var g$url = {
             if (arg.length == 1) g$url.param[arg[0]] = true;
             else g$url.param[arg[0]] = arg[1];
         }
-
-        //test.alert(g$url.param);
-
         if(location.hash)g$url.param.hash = location.hash.substr(1);
         return g$url.param;
     },
     getWxAuth:function(){
-        //alert("getWxAuth");
         var REURI = encodeURIComponent(location.origin + location.pathname),
             STATE = g$url.param.hash||"false";
         return location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf72ed77c92113ec4&redirect_uri="+REURI+"&response_type=code&scope=snsapi_userinfo&state="+STATE+"#wechat_redirect"
     },
     checkUrl:function(){
         var state = g$url.param.state;
-        //alert("checkUrl-state:" + state);
         if(state!="true"){
-            //alert("checkCode:"+g$url.param.code);
             document.cookie = "code="+ (g$url.param.code||null) +"&";
-            //alert("cookie:"+document.cookie);
-            if(state=="false"){
-                //alert("state==false");
-                location.href = location.origin + location.pathname + '?state=true#list'
-            }else{
-                //alert("state:"+state);
-                location.href = location.origin + location.pathname + '?state=true#' + state;
-            }
+            location.href = location.origin + location.pathname +'?state=true'+(state=="false"?'#list':state);
         }else{
             var code = document.cookie.match(/code=([^\b&]*)/);
-            //alert("checkUrl-cookieGet-code:" + code);
             if(code){
                 code = code[1];
                 ROLE.code=code;
                 ROLE.isAuth=true;
                 document.cookie = "code=&";
-                alert(ROLE.code);
             }else{
                 g$url.getWxAuth();
             }
@@ -61,7 +46,7 @@ var ROLE = {
     subscribe:0
 };
 
-//alert(location.href);
+alert(document.cookie);
 if(!g$url.getParam().state){
     g$url.getWxAuth();
 }else{
