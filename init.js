a0b4d8f6ae2dbddeacb21a2e257fcf8215926d20
DevilsEyes <DevilsEyes@git.com>
2015-09-09 23:09:27 +0800

    v30

--- init.js
+++ init.js
@@ -1,3 +1,52 @@
+window.test=true;
+
+test['alert'] = function(obj){
+    if(typeof(obj)=='object'){
+        var str='testAlert:\n|';
+        for (var i in obj) {
+
+            if(typeof obj[i] == 'function'){
+                str += '  ' + i + ':isFunction;';
+            }
+            else{
+                str += '  ' + i + ': ' + obj[i] + '; ';
+            }
+
+            if (typeof obj[i] == 'object') {
+
+                str += '{';
+                for (var j in obj[i]) {
+
+                    if(typeof obj[i][j] == 'function'){
+                        str += '  ' + j + ':isFunction;';
+                    }
+                    else{
+                        str += '  ' + j + ': ' + obj[i][j] + '; ';
+                    }
+
+                    if (typeof obj[i][j] == 'object') {
+                        str += '{';
+
+                        for (var k in obj[i][j]) {
+                            if(typeof obj[i][j][k] == 'function'){
+                                str += '  ' + k + ':isFunction;';
+                            }
+                            else{
+                                str += '  ' + k + ': ' + obj[i][j][k] + '; ';
+                            }
+                        }
+                        str += '}';
+                    }
+                }
+                str += '}';
+            }
+        }
+        alert(str);
+    }else{
+        alert(obj);
+    }
+};
+
 var g$url = {
     param:{},
     getParam:function(){
