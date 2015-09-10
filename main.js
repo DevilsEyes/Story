f2d77d0a8d05ddc09c12325c7e5f9452b3454dc2
DevilsEyes <DevilsEyes@git.com>
2015-09-10 12:15:07 +0800

    v44

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 var baseUrl = 'http://182.92.161.173:5588/activities/productVote/';
-
+test.alert('code:'+ROLE.code);
 ex.jsonp({
-    url: '',//获取openid等一系列参数的url
+    url: 'http://activity.meizhanggui.cc/weixinAuth2/userInfo?_method=GET',//获取openid等一系列参数的url
     data: {
         code: ROLE.code
     },
     success: function (obj) {
+        test.alert(obj);
         if (obj.success) {
 
             ROLE.openId = obj.data.openId;
@@ -101,7 +102,7 @@ var WX = {
                 });
             };
         $.ajax({
-            url: "http://182.92.161.173:5588/weixinAuth?url=" + wxUrl,
+            url: "http://182.92.161.173:5588/weixinAuth2?url=" + wxUrl,
             dataType: "jsonp",
             jsonp: "callback", //传递给请求处理程序或页面的，用以获得jsonp回调函数名的参数名(默认为:callback)
             jsonpCallback: "success_jsonpCallback", //自定义的jsonp回调函数名称，默认为jQuery自动生成的随机函数名
